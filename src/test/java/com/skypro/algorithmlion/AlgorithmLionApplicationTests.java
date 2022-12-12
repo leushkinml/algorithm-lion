cf08026129c021723d829e9a3284ed24c17d0be8
leush <leushkinml@mail.ru>
2022-12-12 16:52:12 +0300

    Initial commit

---
+++ src/test/java/com/skypro/algorithmlion/AlgorithmLionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.algorithmlion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmLionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
